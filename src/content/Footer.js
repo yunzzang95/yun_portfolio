83ae0016816b3a57ff41e51f12ddc5699646e0b4
master <wjddbsgnl100@gmail.com>
2024-11-12 16:31:19 +0900

    pc ver 완료

--- src/content/Footer.js
+++ src/content/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
         <section className="footer">
             <div className="PJWidth">
                 열심히 준비한 제 포트폴리오를 봐주셔서 감사합니다. 사이트에 방문해 주신 모든 분들 행복하세요🍀<br/>
-                github : https://github.com/yunzzang95 | wjddbsgnl100@naver.com
+                <a href="https://github.com/yunzzang95/yun_portfolio" target="_blank" className="colorPT2">yunzzang github</a>
             </div>
         </section>
     );
